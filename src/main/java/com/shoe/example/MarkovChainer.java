e3dc74a1ed37154cf9cfd6593240bd81ecc6d133
Jeremy Green <jeremy@octolabs.com>
2016-02-05 22:17:39 -0600

    Everything is working

--- src/main/java/com/shoe/example/MarkovChainer.java
+++ src/main/java/com/shoe/example/MarkovChainer.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
 import java.util.Hashtable;
+import java.util.Arrays;
+import java.util.StringJoiner;
 
 public class MarkovChainer {
 
@@ -19,12 +21,34 @@ public class MarkovChainer {
   public void train(String s) {
     sentence = s;
     String[] swords = sentence.split(" ");
+    Word previousWord = null;
     for(int i = swords.length -1; i>=0; i--){
       System.out.println("dealing with word: " + swords[i]);
+      Word word = words.get(swords[i]);
+      if(word != null){
+        System.out.println("we had the word");
+        word.addNextWord(previousWord);
+      }else{
+        System.out.println("we didn't have the word");
+        word = new Word(swords[i],previousWord);
+        words.put(swords[i],word);
+      }
+      previousWord = word;
     }
+    beginnings.add(previousWord);
   }
 
   public String generate() {
+    StringJoiner words = new StringJoiner(" ");
+    Collections.shuffle(beginnings);
+    Word word = beginnings.get(0);
+    while(word != null){
+      words.add(word.toString());
+      word = word.nextWord();
+    }
+    String sentence = words.toString();
+    System.out.println("*************");
+    System.out.println(sentence);
     return sentence;
   }
 
@@ -32,17 +56,29 @@ public class MarkovChainer {
     private ArrayList<Word> nextWords;
     private String word;
 
-    public Word(String w){
+    public Word(String w, Word nextWord){
       word = w;
       nextWords = new ArrayList<Word>();
+      nextWords.add(nextWord);
     }
 
-    public void addNextWord(String w){
-      nextWords.add(new Word(w));
+    public void addNextWord(Word w){
+      nextWords.add(w);
     }
 
     public Word nextWord(){
-      return nextWords.get(0);
+      if(nextWords.size() > 0){
+        System.out.println("we have a next word");
+        Collections.shuffle(nextWords);
+        return nextWords.get(0);
+      }else{
+        System.out.print("We do not have a next word");
+        return null;
+      }
+    }
+
+    public String toString(){
+      return word;
     }
   }
 }
